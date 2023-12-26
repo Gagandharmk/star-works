ccf8535fdb6f6e15786a91e3e74f02f9c56d305a
gagan <gagandharmk@gmail.com>
2023-12-26 11:22:47 +0530

    projet start

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1 className="text-red-500 text-center">hello world</h1>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
